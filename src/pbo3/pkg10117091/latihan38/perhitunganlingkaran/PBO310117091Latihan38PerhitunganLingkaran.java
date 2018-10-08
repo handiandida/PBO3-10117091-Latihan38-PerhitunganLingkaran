ab28b5e2918504e2131762d5036e8a24dbf03fc2
Dida Handian <handiandida@gmail.com>
2018-10-08 22:32:28 +0700

    PerhitunganLingakran

---
+++ src/pbo3/pkg10117091/latihan38/perhitunganlingkaran/PBO310117091Latihan38PerhitunganLingkaran.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan38.perhitunganlingkaran;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : 
+*/
+public class PBO310117091Latihan38PerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
